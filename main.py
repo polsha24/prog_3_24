d2f8790af830955bc1114eac115735a95d78f38c
polsha24 <polsha24@yandex.ru>
2024-11-27 12:31:46 +0300

    upd main

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from typing import TypeVar, Iterable
 
 T = TypeVar("T")
 
+
 def first(iterable: Iterable[T]) -> T or None:
     for element in iterable:
         return element
 
+
 print(first(["one", "two"]))  # one
 print(first((100, 200)))  # 200
+print(first({'a': 1, 'b': 2}))
